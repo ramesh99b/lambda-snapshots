fea5cc4d24db1787ff6faacde2f5f82c7262454f
Your Name <you@example.com>
2017-09-11 10:07:55 +0100

    test

---
+++ lambda-snapshots.py
@@ -0,0 +1,21 @@
+import boto3
+
+def lambda_handler(event, context):
+
+    volumes = {
+        "jenkins" : "vol-02085c8c7e3a46b85"
+    }
+    
+    Client = boto3.client('ec2')
+    
+    for volume in volumes:
+        snapshot = Client.create_snapshot(
+          Description='volume %s' % volume,
+          VolumeId=volumes[volume],
+          DryRun=False
+        )
+    
+        if not snapshot:
+            return 'Something happened'    
+
+    return True
